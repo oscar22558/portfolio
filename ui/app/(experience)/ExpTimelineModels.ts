abff1e43dbf3627a4dbede929dae53047572f541
Oscar Shum <oscar22558oscar@gmail.com>
2023-07-21 15:30:35 -0400

    Minor change in experience time line content

--- ui/app/(experience)/ExpTimelineModels.ts
+++ ui/app/(experience)/ExpTimelineModels.ts
@@ -2,9 +2,9 @@ export const ExpTimelineModels = [
     {
         leftTitle: "Research Assistant II (Software Engineer)",
         leftContent: "2021 - 2023",
-        rightTitle: "The department of electrical and electronic engineering, The University of Hong Kong",
+        rightTitle: "The University of Hong Kong",
         rightContent: [
-            "Developed a scalable and maintainable REST api services for an exercise management platform.",
+            "Developed a scalable and maintainable REST api services for an student exercise management solution.",
             "Developed a senior-friendly and easy-to-use mobile app for stroke patients."
         ]
     }
