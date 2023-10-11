cd42526470324d2851e67ee40d3a39bb87a096c2
Oscar Shum <oscar22558oscar@gmail.com>
2023-10-11 02:06:17 -0400

    Updated item border and shadow in experience section

--- ui/app/(experience)/TimelineItem.tsx
+++ ui/app/(experience)/TimelineItem.tsx
@@ -11,7 +11,7 @@ export function TimelineItem({model}: Props){
     const {title, period, descriptions, organization, type} = model
     const icon = type === "work" ? faBriefcase : faGraduationCap
     return (
-        <div className="flex flex-col mt-8 border-[#0ea5e9] border-[2px] rounded-xl p-4 md:p-8 md:flex-row md:mt-8">
+        <div className="flex flex-col mt-8 shadow-lg p-8 md:flex-row md:mt-8">
             <div className="inline-block mb-[8px] md:mb-0">
                 <div className="mt-[10px] bg-[#0ea5e9] rounded-full w-[60px] h-[60px] relative">
                     <FontAwesomeIcon icon={icon} style={{color: "#fff", width: "25px", height: "25px"}}  className="absolute top-[30px] left-[30px] ml-[-13px] mt-[-13px] w-[25px] h-[25px]"/>
